4287ee7b81dc990c8b8d71f03accad2092f0724d
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2014-02-17 14:00:25 +0100

    Fix ticks variable name.

--- chipKIT-cmod-leds/src/delay.c
+++ chipKIT-cmod-leds/src/delay.c
@@ -9,8 +9,8 @@ delay_us(uint32_t delay)
 {
 	uint32_t ticks;	/* 1 core tick = 2 SYS ticks */
 
-	us_ticks = (F_CPU/1000000)/2;
+	ticks = (F_CPU/1000000)/2;
 	WriteCoreTimer(0);
 
-	while(ReadCoreTimer() < delay*us_ticks); /* do nothing */
+	while(ReadCoreTimer() < delay*ticks); /* do nothing */
 } 
